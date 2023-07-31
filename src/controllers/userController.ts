c3954badc8d37b6d13394fcde5be5f7101a9bf26
Yaqub Tawab Olamilekan <maktee2002@gmail.com>
2023-07-31 18:12:15 +0100

    Register user controller created

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,11 @@
 import { Response, Request } from "express";
+import Jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { User, UserModel } from "../models/userModel";
 /**
  * Register new user
  * @desc register new user  
-   @route POST /user/register
+   @route POST nkunzi/user/register
    @access Public
  * @param req 
  * @param res 
@@ -17,6 +19,12 @@ export const registerUser = async (req: Request, res: Response) => {
       message: "Fill in all fields",
       status: "Invalid",
     });
+    if (password < 6) {
+      res.status(400).json({
+        message: "Password need to be greater than 6",
+        status: "Invalid",
+      });
+    }
   }
   const userExits = await UserModel.findOne({ email });
   if (userExits) {
@@ -25,30 +33,92 @@ export const registerUser = async (req: Request, res: Response) => {
       status: "Invalid",
     });
   }
- try {
-     const user = await UserModel.create({
-       firstName ,
-       secondName ,
-       email ,
-       password,
-     })
-     res.status(200).json({
-        message : "User created successfully" ,
-        _id : user.id ,
-         name : user.firstName + user.secondName ,
-         email : user.email ,
-        //  token : generateToken()
-        
-     })
- } catch (error) {
-    console.error('Error creating user:', error);
-    res.status(500).json({
-        message : "Failed to create user"
-    })
 
- }
+  //hash passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  try {
+    const user = await UserModel.create({
+      firstName,
+      secondName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (user) {
+      res.status(201).json({
+        message: "User created successfully",
+        _id: user.id,
+        name:  `${user.firstName}${user.secondName}`,
+        email: user.email,
+        token : generateToken(user._id)
+      });
+    }
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({
+      message: "Failed to create user",
+    });
+  }
 };
 
-const generateToken = (id: string) : any => {
-      return 
+/**
+ *login new user
+ * @desc login  user  
+   @route POST /nkunzi/user/login
+   @access Public
+ * @param req 
+ * @param res 
+ * @returns Response
+ */
+
+export const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({
+      message: "Fill in all credentials",
+      status: "Invalid",
+    });
+  }
+  const user = await UserModel.findOne({ email });
+  if (user && user.password == password) {
+    res.status(200).json({
+      _id: user.id,
+      name: `${user.firstName} ${user.secondName}`,
+      email: user.email,
+      password : password ,
+      token : generateToken(user._id)
+    });
+  } else {
+    return res.status(400).json({
+      message: "Invalid credentials",
+    });
+  }
+};
+
+/**
+ *Get a new user
+ * @desc Get user  
+   @route POST /nkunzi/user/me
+   @access Private 
+ * @param req 
+ * @param res 
+ * @returns Response
+ */
+export const getUser =async (req: Request , res : Response) => {
+    
 }
+
+/**
+ *Jwt
+ * @desc  Generate jwt token  
+   @access Private
+ * @param id : string
+ * @returns token : string
+ */
+const generateToken = (id: string): any => {
+  return Jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,11 +1,11 @@
 import express , { Router} from "express";
 import { Request , Response } from "express";
-import { registerUser } from "../controllers/userController";
+import { registerUser , loginUser, getUser } from "../controllers/userController";
 
 
 const router : Router =  express.Router();
 router.post("/register", registerUser)
-router.post("/login")
-router.get("me");
+router.post("/login", loginUser)
+router.get("/me" , getUser);
 
 export default router ;
