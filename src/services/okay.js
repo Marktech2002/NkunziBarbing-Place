3c6edd3493a136aedcc72b022722ab7d91f456b1
Yaqub Tawab Olamilekan <61840059+Marktech2002@users.noreply.github.com>
2023-10-17 20:18:51 +0100

    Delete src/services/okay.js

--- src/services/okay.js
+++
@@ -1,8 +0,0 @@
-
-const cron = require('node-cron')
-
-cron.schedule('* * * * *', () => {
-  const date = new Date();
-
-  console.log(`This task is running every minute - ${date.getHours()}:${date.getMinutes()}`);
-});
